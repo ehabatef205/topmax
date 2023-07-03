eb1123f2810d98a931b4cc00f63f7e188b5141ce
a7medabdo6 <a7med.abdo6@gmail.com>
2023-07-03 23:06:14 +0300

    change backend api

--- front_topmax/src/api/axios.js
+++ front_topmax/src/api/axios.js
@@ -1,3 +1,3 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'https://pleasant-outfit-mite.cyclic.app/'
+import axios from "axios";
+axios.defaults.baseURL = "http://5.183.9.124:7000/";
 export default axios;
